f5f1aa190c82558da29938c77957e0e31b1db624
Caio Lucas <caio.lucas@ploomes.com>
2023-03-19 18:30:24 -0300

    Trabalhando com filas

---
+++ Rabbit.Publisher.Api/Controllers/RabbitMensagensController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rabbit.Services.Interfaces;
+using Rabbit.Models.Entities;
+
+namespace Rabbit.Publisher.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RabbitMensagensController : ControllerBase
+    {
+        private readonly IRabbitMensagemService _service;
+
+        public RabbitMensagensController(IRabbitMensagemService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public void AddMensagem(RabbitMensagem mensagem)
+        {
+            _service.SendMensagem(mensagem);
+        }
+    }
+}
---
+++ Rabbit.Services/Interfaces/IRabbitMensagemService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rabbit.Models.Entities;
+
+namespace Rabbit.Services.Interfaces
+{
+    public interface IRabbitMensagemService
+    {
+        void SendMensagem(RabbitMensagem mensagem);
+            
+    }
+}
